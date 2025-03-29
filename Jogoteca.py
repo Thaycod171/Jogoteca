59079818561ed028b828738d50d5d28537f6bd6b
Thaycod171 <nunesthayane057@gmail>
2025-03-29 01:03:45 -0300

    criação de novas rotas

--- Jogoteca.py
+++ Jogoteca.py
@@ -1,10 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, render_template , request
 
+
+class Jogo:
+    def __init__(self , nome , categoria , console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+Jogo1 = Jogo('Valorant' , 'tiro' , 'pc')
+Jogo2 = Jogo('Genshin' , 'rpg' , 'pc/mobile')
+Jogo3 = Jogo('lol' , 'magia' , 'pc/mobile')
+
+lista = [Jogo1 , Jogo2 , Jogo3]
+    
 app = Flask(__name__)
+
 
 @app.route('/inicio')
 def ola():
-    lista = ['mario' , 'chash' , 'valorant']
+
     return render_template('lista.html' , titulo='Jogos' , Jogos=lista)
 
+
+@app.route('/novo')
+def novo():
+    return render_template('novo.html' , titulo ='Novo Jogo')
+
+
+@app.route('/criar')
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+
+    jogo = Jogo(nome , categoria , console)
+    lista.append(jogo)
+    return render_template('listahtml' , titulo='Jogo' , Jogos=lista)
 app.run()
